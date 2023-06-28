536803b7e6f55f3e1ba8d237d2d2576071aaeb3e
Deren <de2356@columbia.edu>
2023-06-28 13:16:08 -0400

    update for toy3 syntax change

--- ipcoal/smc/likelihood/utils.py
+++ ipcoal/smc/likelihood/utils.py
@@ -8,14 +8,14 @@ import numpy as np
 import ipcoal
 
 
-def iter_spans_and_trees(model: ipcoal.Model, locus: Optional[int]=None) -> Iterator[Tuple[int, toytree.ToyTree]]:
+def iter_spans_and_trees(model: ipcoal.Model, locus: Optional[int] = None) -> Iterator[Tuple[int, toytree.ToyTree]]:
     """Yield ToyTree objects parsed from newick strings."""
     data = model.df[model.df.locus == locus] if locus is not None else model.df
     for span, newick in data[["nbps", "genealogy"]].values:
         yield (span, toytree.tree(newick))
 
 
-def iter_spans_and_topologies(model: ipcoal.Model, locus: Optional[int]=None) -> Iterator[int]:
+def iter_spans_and_topologies(model: ipcoal.Model, locus: Optional[int] = None) -> Iterator[int]:
     """Return an array of genealogical topology lengths.
 
     Parameters
@@ -30,45 +30,45 @@ def iter_spans_and_topologies(model: ipcoal.Model, locus: Optional[int]=None) ->
     for span, gtree in iter_spans_and_trees(model, locus=locus):
         interval += span
         if current:
-            new = gtree.get_topology_id(exclude_root=False)
-            if current != new:
+            new = gtree.get_topology_id(include_root=True)
             # if current.distance.get_treedist_rf(gtree, False):
+            if current != new:
                 yield interval, gtree
                 # current = gtree
                 current = new
                 interval = 0
         else:
             # current = gtree
-            current = gtree.get_topology_id(exclude_root=False)
+            current = gtree.get_topology_id(include_root=True)
 
 
 def iter_unique_topologies_from_genealogies(
     genealogies: Sequence[toytree.ToyTree],
-    #average_branch_lengths: bool=False,
-    ) -> Sequence[toytree.ToyTree]:
+    # average_branch_lengths: bool=False,
+) -> Sequence[toytree.ToyTree]:
     """Returns the genealogy at each topology change."""
 
     # initial tree
     current = genealogies[0]
-    cidx = current.get_topology_id(exclude_root=False)
+    cidx = current.get_topology_id(include_root=True)
     tree_bunch = [current]
 
     # iterate over genealogies
     for gtree in genealogies[1:]:
-        nidx = gtree.get_topology_id(exclude_root=False)
+        nidx = gtree.get_topology_id(include_root=True)
         if cidx != nidx:
 
             # optional: average branch lens across genealogies
             # deprecated for now, need to disallow negative blens.
             # if average_branch_lengths:
-                # print(f'tree_bunch: {len(tree_bunch)}')
-                # elens = {}
-                # for nid in range(current.ntips, current.nnodes):
-                #     hei = np.mean([g[nid].height for g in tree_bunch])
-                #     elens[nid] = hei
-                # print(elens)
-                # current = current.set_node_data("height", mapping=elens)
-        
+            #     print(f'tree_bunch: {len(tree_bunch)}')
+            #     elens = {}
+            #     for nid in range(current.ntips, current.nnodes):
+            #         hei = np.mean([g[nid].height for g in tree_bunch])
+            #         elens[nid] = hei
+            #     print(elens)
+            #     current = current.set_node_data("height", mapping=elens)
+
             yield current
             current = gtree
             cidx = nidx
@@ -77,7 +77,7 @@ def iter_unique_topologies_from_genealogies(
             tree_bunch.append(gtree)
 
 
-def get_topology_interval_lengths(model: ipcoal.Model, locus: Optional[int]=None) -> np.ndarray:
+def get_topology_interval_lengths(model: ipcoal.Model, locus: Optional[int] = None) -> np.ndarray:
     """Return an array of genealogical topology lengths.
 
     Parameters
@@ -87,7 +87,7 @@ def get_topology_interval_lengths(model: ipcoal.Model, locus: Optional[int]=None
         such as `sim_trees`, `sim_loci`, etc. such that it has data
         in its `.df` attribute.
     """
-    return np.array([i[0] for i in iter_spans_and_topologies(model, locus=locus)])        
+    return np.array([i[0] for i in iter_spans_and_topologies(model, locus=locus)])
 
 
 if __name__ == "__main__":
@@ -105,4 +105,4 @@ if __name__ == "__main__":
 
     GENEALOGIES = toytree.mtree(MODEL.df.genealogy)
     for g in iter_unique_topologies_from_genealogies(GENEALOGIES):
-        print(g.get_topology_id(exclude_root=0), g.get_node_data("height"))
+        print(g.get_topology_id(include_root=True), g.get_node_data("height"))
