4edc308ea84c2a44d9fa3a3f16a3da053bc8468c
Deren <de2356@columbia.edu>
2023-07-01 15:45:00 -0400

    sim_trees faster and w/ parallel option

--- ipcoal/core/sim_trees.py
+++ ipcoal/core/sim_trees.py
@@ -2,21 +2,37 @@
 
 """Simulate `nloci` unlinked genealogies.
 
+Parallel code returns same result as non-parallel using rng_trees.
 """
 
-from typing import TypeVar
+from typing import TypeVar, Sequence, Tuple, Mapping
+from concurrent.futures import ProcessPoolExecutor
+from loguru import logger
 import numpy as np
 import pandas as pd
 from ipcoal.utils.utils import IpcoalError
 
+logger = logger.bind(name="ipcoal")
 Model = TypeVar("Model")
+COLUMNS = ['locus', 'start', 'end', 'nbps', 'nsnps', 'tidx', 'genealogy']
 
 
-def sim_trees(model: Model, nloci: int, nsites: int, precision: int=14) -> None:
-    """Simulate unlinked genealogies. 
+def sim_trees(
+    model: Model,
+    nloci: int,
+    nsites: int,
+    precision: int = 14,
+    nproc: int = 1,
+) -> None:
+    """Simulate unlinked genealogies.
 
-    See `ipcoal.Model.sim_trees` docstring.
+    Parallelization provides less than linear speed-up. Default is
+    nproc=1. Only used and useful for very large simulations.
     """
+    # clear any existing stored tree sequences
+    model.ts_dict = {}
+    model.seqs = np.array([])
+
     # check conflicting args
     if model._recomb_is_map:
         if nsites:
@@ -25,51 +41,163 @@ def sim_trees(model: Model, nloci: int, nsites: int, precision: int=14) -> None:
                 "the recomb_map also specifies nsites. To use a recomb_map"
                 "specify nsites=None.")
         nsites = model.recomb.sequence_length
+    if model.store_tree_sequences and (nproc > 1):
+        nproc = 1
+        logger.warning(
+            "Cannot use nproc>1 with store_tree_sequences=True. Using nproc=1.")
 
-    # clear any existing stored tree sequences
-    model.ts_dict = {}
+    # run single core simulation. Stores results to Model and returns None
+    if nproc == 1:
+        data, ts_dict = _sim_trees(model, nloci, nsites, precision)
+        model.df = data
+        model.ts_dict = ts_dict
+        model._reset_random_generators()
+        return None
 
+    # cache some model elements that are not pickle-able and not used.
+    _old_substitution_model = model.subst_model
+    rng_trees = model.rng_trees
+    model.rng_trees = None
+    model.rng_muts = None     # doesn't matter in sim_trees
+    model.subst_model = None  # doesn't matter in sim_trees
+
+    chunksize = int(nloci / nproc) + (nloci % nproc)
+    chunksize = max(chunksize, 100)
+    rasyncs = {}
+    with ProcessPoolExecutor(max_workers=nproc) as pool:
+        for chunk in range(0, nloci, chunksize):
+            # get number of loci to sim on this engine
+            low = chunk
+            high = min(nloci, chunk + chunksize)
+            chunkloci = high - low
+
+            # generate seeds from rng_trees
+            seeds = rng_trees.integers(2 ** 31, size=chunkloci)
+
+            # send job to run
+            args = (model, chunkloci, nsites, precision, seeds)
+            rasyncs[low] = pool.submit(_sim_trees, *args)
+
+    # store dataframe to Model and clear any existing sequences
     datalist = []
-    for lidx in range(nloci):
-        msgen = model._get_tree_sequence_generator(nsites)
-        tree_seq = next(msgen)
-        breaks = [int(i) for i in tree_seq.breakpoints()]
-        starts = breaks[0:len(breaks) - 1]
-        ends = breaks[1:len(breaks)]
-        lengths = [i - j for (i, j) in zip(ends, starts)]
-
-        data = pd.DataFrame({
-            "start": starts,
-            "end": ends,
-            "nbps": lengths,
-            "nsnps": 0,
-            "tidx": 0,
-            "locus": lidx,
-            "genealogy": "",
-            },
-            columns=[
-                'locus', 'start', 'end', 'nbps',
-                'nsnps', 'tidx', 'genealogy'
-            ],
-        )
-
-        # iterate over the index of the dataframe to sim for each genealogy
-        for mstree in tree_seq.trees():
-            # convert nwk to original names
-            nwk = mstree.newick(node_labels=model.tipdict, precision=precision)
-            data.loc[mstree.index, "genealogy"] = nwk
-            data.loc[mstree.index, "tidx"] = mstree.index
+    for low, future in rasyncs.items():
+        data, ts_dict = future.result()
+        data.locus += low
         datalist.append(data)
+    model.df = pd.concat(datalist, axis=0)
+    model.df = model.df.reset_index(drop=True)
+
+    # re-set generators on objects
+    model._reset_random_generators()
+    model.subst_model = _old_substitution_model
+    return None
+
+
+def _sim_trees(
+    model: Model,
+    nloci: int,
+    nsites: int,
+    precision: int = 14,
+    seeds: Sequence[int] = None,
+) -> Tuple[pd.DataFrame, Mapping[int, 'TreeSequence']]:
+    """Simulate unlinked genealogies.
+
+    This is the main loop that can be run on parallel engines.
+    """
+    # optionally store tree_sequences when not run remotely
+    ts_dict = {}
+
+    # optionally use pre-generated seeds when run remotely
+    if seeds is None:
+        seeds = model.rng_trees.integers(2**31, size=nloci)
+    else:
+        assert len(seeds) == nloci
+
+    # generate a new ts for each independent locus
+    datalist = []
+    for lidx, seed in zip(range(nloci), seeds):
+        # init ts generator and get first ts
+        msgen = model._get_tree_sequence_generator(nsites, seed=seed)
+        tree_seq = next(msgen)
+
+        # iterate over linked trees in ts
+        for tree in tree_seq.trees():
+            # get interval for this tree
+            ival = tree.get_interval()
+            index = tree.get_index()
+            # get newick for this tree w/ original names
+            nwk = tree.newick(node_labels=model.tipdict, precision=precision)
+            # store all to a list
+            row = [lidx, int(ival.left), int(ival.right), int(ival.span), 0, index, nwk]
+            datalist.append(row)
 
         # store the tree_sequence
         if model.store_tree_sequences:
-            model.ts_dict[lidx] = tree_seq
+            ts_dict[lidx] = tree_seq
 
-    # concatenate all of the genetree dfs
-    data = pd.concat(datalist)
-    data = data.reset_index(drop=True)
-    model.df = data
-    model.seqs = np.array([])
+    # organize into a dataframe and return
+    return pd.DataFrame(datalist, columns=COLUMNS), ts_dict
+
+
+# DEPRECATED FOR NEW sim_trees and _sim_trees combo that allows for parallel.
+# def sim_trees(model: Model, nloci: int, nsites: int, precision: int = 14) -> None:
+#     """Simulate unlinked genealogies.
+
+#     See `ipcoal.Model.sim_trees` docstring.
+#     """
+#     # check conflicting args
+#     if model._recomb_is_map:
+#         if nsites:
+#             raise IpcoalError(
+#                 "Both nsites and recomb_map cannot be used together since"
+#                 "the recomb_map also specifies nsites. To use a recomb_map"
+#                 "specify nsites=None.")
+#         nsites = model.recomb.sequence_length
+
+#     # clear any existing stored tree sequences
+#     model.ts_dict = {}
+
+#     datalist = []
+#     for lidx in range(nloci):
+#         msgen = model._get_tree_sequence_generator(nsites)
+#         tree_seq = next(msgen)
+#         breaks = [int(i) for i in tree_seq.breakpoints()]
+#         starts = breaks[0:len(breaks) - 1]
+#         ends = breaks[1:len(breaks)]
+#         lengths = [i - j for (i, j) in zip(ends, starts)]
+
+#         data = pd.DataFrame({
+#             "start": starts,
+#             "end": ends,
+#             "nbps": lengths,
+#             "nsnps": 0,
+#             "tidx": 0,
+#             "locus": lidx,
+#             "genealogy": "",
+#         },
+#             columns=[
+#                 'locus', 'start', 'end', 'nbps',
+#                 'nsnps', 'tidx', 'genealogy'
+#         ],
+#         )
+
+#         # iterate over the index of the dataframe to sim for each genealogy
+#         for mstree in tree_seq.trees():
+#             # convert nwk to original names
+#             nwk = mstree.newick(node_labels=model.tipdict, precision=precision)
+#             data.loc[mstree.index, "genealogy"] = nwk
+#             data.loc[mstree.index, "tidx"] = mstree.index
+#         datalist.append(data)
+
+#         # store the tree_sequence
+#         if model.store_tree_sequences:
+#             model.ts_dict[lidx] = tree_seq
+
+#     # concatenate all of the genetree dfs
+#     data = pd.concat(datalist)
+#     data = data.reset_index(drop=True)
+#     model.df = data
+#     model.seqs = np.array([])
 
 
 if __name__ == "__main__":
@@ -78,6 +206,6 @@ if __name__ == "__main__":
     import ipcoal
 
     TREE = toytree.rtree.unittree(ntips=6, treeheight=1e6)
-    MODEL = ipcoal.Model(TREE, Ne=1e4)
-    sim_trees(MODEL, 10, 10)
+    MODEL = ipcoal.Model(TREE, Ne=1e4, seed_trees=123)
+    sim_trees(MODEL, 10, 1e4)
     print(MODEL.df)
